428cd338cc1c391afb766d31e27aa14a7b4cea34
Adrar94 <Adrar187@Gmail.com>
2014-12-08 15:56:07 +0000

    Graph setBlot

--- src/jvm/storm/starter/TwitterTest.java
+++ src/jvm/storm/starter/TwitterTest.java
@@ -16,13 +16,13 @@ import storm.starter.bolt.RollingCountBolt;
 import storm.starter.bolt.TotalRankingsBolt;
 import storm.starter.spout.RandomSentenceSpout;
 import storm.starter.spout.TwitterSampleSpout;
+import storm.starter.util.GraphBolt;
 
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Created by gperinazzo on 28/11/2014.
- */
+// Created by gperinazzo on 28/11/2014
+
 public class TwitterTest {
 
     public static class PrinterTweetBolt extends BaseBasicBolt {
@@ -34,8 +34,8 @@ public class TwitterTest {
 
         @Override
         public void declareOutputFields(OutputFieldsDeclarer ofd) {
-        }
 
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -44,29 +44,30 @@ public class TwitterTest {
 
         builder.setSpout("spout", new TwitterSampleSpout(), 1);
         builder.setBolt("split", new SplitBolt(), 3).shuffleGrouping("spout");
-        
-        //Hashtags
+
+        // Hashtags
         builder.setBolt("hashtag-counter", new RollingCountBolt(9, 3), 3).fieldsGrouping("split", "hashtags", new Fields("hashtag"));
         builder.setBolt("hashtag-intermediate-ranking", new IntermediateRankingsBolt(10), 3).fieldsGrouping("hashtag-counter", new Fields("obj"));
         builder.setBolt("hashtag-total-ranking", new TotalRankingsBolt(10)).globalGrouping("hashtag-intermediate-ranking");
         builder.setBolt("hashtag-ranking-print", new RankingPrinterBolt("HASHTAG_RANKING.txt")).shuffleGrouping("hashtag-total-ranking");
 
-        //Most active user
+        // Most active user
         builder.setBolt("user-counter", new RollingCountBolt(9, 3), 3).fieldsGrouping("split", "usernames", new Fields("username"));
         builder.setBolt("user-intermediate-ranking", new IntermediateRankingsBolt(10), 3).fieldsGrouping("user-counter", new Fields("obj"));
         builder.setBolt("user-total-ranking", new TotalRankingsBolt(10)).globalGrouping("user-intermediate-ranking");
         builder.setBolt("user-ranking-print", new RankingPrinterBolt("USER_RANKING.txt")).shuffleGrouping("user-total-ranking");
 
-        //Mentions
+        // Mentions
         builder.setBolt("mentions-counter", new RollingCountBolt(9, 3), 3).fieldsGrouping("split", "mentions", new Fields("mention"));
         builder.setBolt("mentions-intermediate-ranking", new IntermediateRankingsBolt(10), 3).fieldsGrouping("mentions-counter", new Fields("obj"));
         builder.setBolt("mentions-total-ranking", new TotalRankingsBolt(10)).globalGrouping("mentions-intermediate-ranking");
         builder.setBolt("mentions-ranking-print", new RankingPrinterBolt("MENTIONS_RANKING.txt")).shuffleGrouping("mentions-total-ranking");
 
+        // Graph
+        builder.setBolt("graph-connections", new GraphBolt()).fieldsGrouping("split", "links", new Fields("mention"));
 
         Config conf = new Config();
         conf.setDebug(true);
-
 
         if (args != null && args.length > 0) {
             conf.setNumWorkers(3);
@@ -86,17 +87,12 @@ public class TwitterTest {
             try {
                 Thread.sleep(20000);
             } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
                 e.printStackTrace();
             }
+
             cluster.shutdown();
 
             //cluster.killTopology("word-count");
-
-            cluster.shutdown();
-
-
-
         }
     }
 }
